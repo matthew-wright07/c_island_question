d568b10607530e6b9d0e2bef621fd7b27f6c9427
Matthew <matthewjwright07@gmail.com>
2025-06-15 11:51:20 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int island[10][2] = {
+        {0,4},
+        {1,6},
+        {2,8},
+        {3,6},
+        {4,2},
+        {5,4},
+        {6,10},
+        {7,8},
+        {8,4},
+        {9,0}
+    };
+
+    for (int i=10;i>=0;i--){
+        for (int j=0;j<10;j++){
+            if (island[j][1]>=i){
+                printf("  #  ");
+            }else{
+                printf("     ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
